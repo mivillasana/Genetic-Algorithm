cfab69caebc86de6f7d937ea3d5b0d6004c5009a
mivillasana <m_v032007@sbcglobal.net>
2015-04-05 16:23:30 -0700

    "Added initialization and eval with print to check values"

--- index.py
+++ index.py
@@ -3,11 +3,29 @@ import random
 
 #Parameter Values
 maxValue = 25
+a = 0
+b = 0
+c = 0
+d = 0
+def f(a,b,c,d):
+	return ((a+2*b+3*c+4*d)-30)
 
-#step 1
-Chromosome1 = [random.ranrange(maxValue),random.ranrange(maxValue),random.ranrange(maxValue),random.ranrange(maxValue)]
-Chromosome2 = [random.ranrange(maxValue),random.ranrange(maxValue),random.ranrange(maxValue),random.ranrange(maxValue)]
-Chromosome3 = [random.ranrange(maxValue),random.ranrange(maxValue),random.ranrange(maxValue),random.ranrange(maxValue)]
-Chromosome4 = [random.ranrange(maxValue),random.ranrange(maxValue),random.ranrange(maxValue),random.ranrange(maxValue)]
-Chromosome5 = [random.ranrange(maxValue),random.ranrange(maxValue),random.ranrange(maxValue),random.ranrange(maxValue)]
-Chromosome6 = [random.ranrange(maxValue),random.ranrange(maxValue),random.ranrange(maxValue),random.ranrange(maxValue)]
+#step 1 Initialization
+Chromosome1 = [a,b,c,d] = [random.randrange(maxValue),random.randrange(maxValue),random.randrange(maxValue),random.randrange(maxValue)]
+F_obj1 = abs(f(a,b,c,d))
+Chromosome2 = [a,b,c,d] = [random.randrange(maxValue),random.randrange(maxValue),random.randrange(maxValue),random.randrange(maxValue)]
+F_obj2 = abs(f(a,b,c,d))
+Chromosome3 = [a,b,c,d] = [random.randrange(maxValue),random.randrange(maxValue),random.randrange(maxValue),random.randrange(maxValue)]
+F_obj3 = abs(f(a,b,c,d))
+Chromosome4 = [a,b,c,d] = [random.randrange(maxValue),random.randrange(maxValue),random.randrange(maxValue),random.randrange(maxValue)]
+F_obj4 = abs(f(a,b,c,d))
+Chromosome5 = [a,b,c,d] = [random.randrange(maxValue),random.randrange(maxValue),random.randrange(maxValue),random.randrange(maxValue)]
+F_obj5 = abs(f(a,b,c,d))
+Chromosome6 = [a,b,c,d] = [random.randrange(maxValue),random.randrange(maxValue),random.randrange(maxValue),random.randrange(maxValue)]
+F_obj6 = abs(f(a,b,c,d))
+print(F_obj1,end='\n')
+print(F_obj2,end='\n')
+print(F_obj3,end='\n')
+print(F_obj4,end='\n')
+print(F_obj5,end='\n')
+print(F_obj6,end='\n')
