d8338e232984690bf160b0c6659b69502a3ae8f6
mivillasana <m_v032007@sbcglobal.net>
2015-04-05 15:56:44 -0700

    "initialization

--- index.py
+++ index.py
@@ -1,1 +1,13 @@
 import math
+import random
+
+#Parameter Values
+maxValue = 25
+
+#step 1
+Chromosome1 = [random.ranrange(maxValue),random.ranrange(maxValue),random.ranrange(maxValue),random.ranrange(maxValue)]
+Chromosome2 = [random.ranrange(maxValue),random.ranrange(maxValue),random.ranrange(maxValue),random.ranrange(maxValue)]
+Chromosome3 = [random.ranrange(maxValue),random.ranrange(maxValue),random.ranrange(maxValue),random.ranrange(maxValue)]
+Chromosome4 = [random.ranrange(maxValue),random.ranrange(maxValue),random.ranrange(maxValue),random.ranrange(maxValue)]
+Chromosome5 = [random.ranrange(maxValue),random.ranrange(maxValue),random.ranrange(maxValue),random.ranrange(maxValue)]
+Chromosome6 = [random.ranrange(maxValue),random.ranrange(maxValue),random.ranrange(maxValue),random.ranrange(maxValue)]
