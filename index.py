81252717c042da2981ed3a341351a6fd48a215ed
mivillasana <m_v032007@sbcglobal.net>
2015-04-05 17:50:30 -0700

    "Made the print outs look nicer, need to figure out how to its going to work for +iterations"

--- index.py
+++ index.py
@@ -7,33 +7,41 @@ a = 0
 b = 0
 c = 0
 d = 0
-Chromosome = [a,b,c,d]
 F_obj = 0
 Fit = 0
 Total = 0
+prob = 0
+cummulative = 0
+Chromosome = [a,b,c,d]
 Chromosomes = [Chromosome,Chromosome,Chromosome,Chromosome,Chromosome,Chromosome]
 F_objs= [F_obj,F_obj,F_obj,F_obj,F_obj,F_obj]
 Fitness = [Fit,Fit,Fit,Fit,Fit,Fit]
+P = [prob,prob,prob,prob,prob,prob]
+C = [cummulative,cummulative,cummulative,cummulative,cummulative,cummulative]
 def f(a,b,c,d):
 	return ((a+2*b+3*c+4*d)-30)
 
-#step 1 and 2 Initialization and Evaluation
+#step 1, 2, and 3 Initialization, Evaluation and Selection
 index = 0
 for index in range(len(Chromosomes)):
         Chromosomes[index] = [a,b,c,d] = [random.randrange(maxValue),random.randrange(maxValue),random.randrange(maxValue),random.randrange(maxValue)]
         F_objs[index] = abs(f(a,b,c,d))
         Fitness[index] = 1/(1+F_objs[index])
-        print("Chromosomes Value for: ",index,"is\n", Chromosomes[index])
-        print("F_obj Value for: ",index,"is\n", F_objs[index])
-        print("Fitness Value for: ",index,"is\n", Fitness[index])
+        print("Chromosomes[",index,"] is: ", Chromosomes[index])
+        print("F_obj[",index,"] is: ", F_objs[index])
+        print("Fitness[",index,"] is: ", Fitness[index],"\n")
 for index in range(len(Chromosomes)):
         Total += Fitness[index]
-
-
+        
 #print total fitness Value
-print("\nPrinting Total Fitness Values:")
-print(Total,end='\n')
+print("\nPrinting Summed Fitness Values:",Total,end='\n\n')
 
 #Calculate Probability for each Chromosome
-#P1 = Fitness1/Total
+index = 0
+for index in range(len(P)):
+       P[index] = Fitness[index]/Total
+       print("Probability Value for ",index,"is: ", P[index],"\n")
 
+#Calculate Cummlative probability of selection process
+# i have no idea why this is here.. 
+for index in range(len(P)):
